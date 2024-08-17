cc29fcd1ba627b47e63ab285c9d8800b3a521a45
amir-ben-shimol <amir.benshi@gmail.com>
2024-08-17 16:21:56 +0300

    chore: Update app component name and labels

--- terraform/k8s/[k8s]-website-deployment.tf
+++ terraform/k8s/[k8s]-website-deployment.tf
@@ -4,7 +4,7 @@ resource "kubernetes_deployment_v1" "website" {
     namespace = "default"
 
     labels = {
-      app = "website"
+      app = "amir-ben-shimol-website"
     }
   }
 
@@ -13,21 +13,21 @@ resource "kubernetes_deployment_v1" "website" {
 
     selector {
       match_labels = {
-        app = "website"
+        app = "amir-ben-shimol-website"
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "website"
+          app = "amir-ben-shimol-website"
         }
       }
 
       spec {
         container {
           image             = "${data.aws_ecr_repository.website.repository_url}:${var.website_docker_image_tag}"
-          name              = "website"
+          name              = "amir-ben-shimol-website"
           image_pull_policy = "Always"
 
           liveness_probe {
@@ -42,7 +42,7 @@ resource "kubernetes_deployment_v1" "website" {
 
           port {
             container_port = 80
-            name           = "website"
+            name           = "amir-ben-shimol-website"
           }
 
           resources {
