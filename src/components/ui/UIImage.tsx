174c41ee9e2b6215b63b9ff34c7ece632fe5cd8e
amir-ben-shimol <amir.benshi@gmail.com>
2024-08-18 08:14:48 +0300

    chore: Update UIImage component to use unoptimized images

--- src/components/ui/UIImage.tsx
+++ src/components/ui/UIImage.tsx
@@ -5,5 +5,5 @@ import Image, { type ImageProps } from 'next/image';
 import { cn } from '@/utils/class-name';
 
 export const UIImage: React.FC<ImageProps> = (props) => {
-	return <Image {...props} quality={100} className={cn('transition-all duration-500 ease-in-out', props.className)} />;
+	return <Image {...props} quality={100} unoptimized className={cn('transition-all duration-500 ease-in-out', props.className)} />;
 };
