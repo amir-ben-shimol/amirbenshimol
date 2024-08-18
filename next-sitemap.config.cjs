266cb984f85648c46f8a8dd38ec5014f5906fd17
amir-ben-shimol <amir.benshi@gmail.com>
2024-08-18 22:19:33 +0300

    chore: Update next-sitemap to version 4.2.3

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: 'https://amirbenshimol.com', // Replace with your domain
+	generateRobotsTxt: true, // Generate a robots.txt file
+	sitemapSize: 7000, // Adjust this according to your needs
+};
