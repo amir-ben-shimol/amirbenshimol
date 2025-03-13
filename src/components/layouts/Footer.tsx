b83d9d194633632714d519f0ada769cbc6b3224e
amir-ben-shimol <amir.benshi@gmail.com>
2025-03-13 20:37:35 +0100

    feat: update footer visibility and improve global styles with background variables

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -8,10 +8,10 @@ export const Footer = (props: Props) => {
 	return (
 		<footer
 			className={cn('p-4 text-center text-gray-600 dark:text-gray-400', {
-				'fixed bottom-0 left-0 right-0 z-30': props.floating,
+				'fixed bottom-0 left-0 right-0 z-30 hidden sm:block': props.floating,
 			})}
 		>
-			<p>© 2025 Developed & Designed by Amir Ben Shimol. All rights reserved.</p>
+			<p>© 2025 Amir Ben Shimol. All rights reserved.</p>
 		</footer>
 	);
 };
--- src/components/wrappers/AppWrapper.tsx
+++ src/components/wrappers/AppWrapper.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const AppWrapper = (props: Props) => {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body suppressHydrationWarning className="bg-slate-100 dark:bg-slate-950">
+		<html lang="en">
+			<body suppressHydrationWarning>
 				<NextTopLoader
 					color="rgb(34 197 94)"
 					initialPosition={0.08}
