7887171980d259b2da4a64f965e80b2675c33b60
amir-ben-shimol <amir.benshi@gmail.com>
2025-03-13 20:00:34 +0100

    feat: add blog post on creating YouTube-like loader in React routing

--- src/lib/data/blogs/index.tsx
+++ src/lib/data/blogs/index.tsx
@@ -2,8 +2,17 @@ import type { Blog } from '@/types/ui/blog';
 
 import CssScssVsTailwindWhenToUseWhat from './content/css-scss-vs-tailwind-when-to-use-what.mdx';
 import NextJsVsReactWhenToUseWhat from './content/nextjs-vs-react-when-to-use-what.mdx';
+import ReactCreateYoutubeLikeLoaderWhenRoutingWithoutScreenFlickering from './content/react-create-youtube-like-loader-when-routing-without-screen-flickering.mdx';
 
 export const blogs: Blog[] = [
+	{
+		id: 'react-create-youtube-like-loader-when-routing-without-screen-flickering',
+		title: 'React: Create YouTube-like loader when routing without screen flickering',
+		description: 'Create page-transition loader, including async data fetching',
+		date: new Date('2025-03-13'),
+		readingTime: '10 min read',
+		contentElement: <ReactCreateYoutubeLikeLoaderWhenRoutingWithoutScreenFlickering />,
+	},
 	{
 		id: 'nextjs-vs-react-when-to-use-what',
 		title: 'Next.js vs. React: When to Use What',
