2a40dbe904d8b2f82fe449e17f0a1923e791af44
amir-ben-shimol <amir.benshi@gmail.com>
2026-02-20 22:30:06 +0200

    fix: update footer copyright year dynamically to reflect the current year

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -5,13 +5,15 @@ type Props = {
 };
 
 export const Footer = (props: Props) => {
+	const currentYear = new Date().getFullYear();
+
 	return (
 		<footer
 			className={cn('p-4 text-center text-gray-600 dark:text-gray-400', {
 				'fixed bottom-0 left-0 right-0 z-30 hidden sm:block': props.floating,
 			})}
 		>
-			<p>© 2026 Amir Ben Shimol. All rights reserved.</p>
+			<p>{`© ${currentYear} Amir Ben Shimol. All rights reserved.`}</p>
 		</footer>
 	);
 };
